6b391086dc544a63713ffaec411abeaf5a4e01d0
Hemanth5544 <rachapalli.hemanth5544@gmail.com>
2025-03-31 21:24:34 +0530

    document apis

--- controllers/documents.js
+++ controllers/documents.js
@@ -87,3 +87,69 @@ const FindDocument = async (req,res) => {
         return res.json(error)
     }
 }
+
+
+const FindDocuments = async (req,res) => {
+    try {
+
+        const documents = await Documents.find({
+            $or : [
+                {document_creator : req.user.id},
+                {"document_contributors.contributor_id" : req.user.id}
+            ]
+        })
+        .populate("document_contributors.contributor_id" , "username email")
+        .populate("document_creator" , "username email")
+
+    
+        return res.status(200).json({
+            status : 200,
+            successful : true,
+            documents
+        }) 
+
+    } catch (error) {
+        console.log(error);
+        return res.json(error)
+    }
+}
+
+
+const UpdateDocumentContent = async (req,res) => {
+    try {
+        const document_id = req.params.document_id
+        if(!document_id){
+            return res.status(400).json({
+                status : 400,
+                successful : false,
+                message : "document_id parameter is not provided"
+            }) 
+        }
+
+        const content = req.body.content
+        const document = await Documents.findOneAndUpdate(
+            { document_id : document_id}, 
+            { $set: { document_content: content } },
+            { runValidators: true } 
+        );
+
+        if(!document){
+            return res.status(404).json({
+                status : 404,
+                successful : false,
+                message : "document not found!"
+            })   
+        }
+
+        return res.status(200).json({
+            status : 200,
+            successful : true,
+            message : "document has been updated"
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.json(error)
+    }
+}
+
