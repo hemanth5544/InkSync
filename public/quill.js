aadf70a80c605dcc443f16b55c513e5f91b03484
Hemanth5544 <rachapalli.hemanth5544@gmail.com>
2025-04-04 14:19:34 +0530

    quill js changes

--- public/quill.js
+++ public/quill.js
@@ -70,3 +70,71 @@ const GetUserRole = async () => {
     return data.role
 }
 
+
+const UiElementsAcessControl = async () => {
+    const UserRole = await GetUserRole()
+    //ui elements
+    const invite_btn = document.getElementById("invite-btn")
+    const save_as_btn = document.getElementById("save-as-btn")
+
+
+    if(UserRole !== "creator" && UserRole !== "editor"){
+        invite_btn.disabled = true;
+    }
+
+    if(UserRole !== "creator" && UserRole !== "editor"){
+        save_as_btn.disabled = true;
+    }
+    
+}
+
+
+const CanWrite = async () => {
+    const AllwedRoles = ["creator","editor"]
+    const UserRole = await GetUserRole()
+    
+    if(!AllwedRoles.includes(UserRole)){
+        quill.disable()
+    }
+}
+
+
+const GetDocumentContributors = async () => {
+    const response = await fetch(`/api/documents/GetDocumentContributors/${documentId}`)
+    const data = await response.json()
+    
+    const user_list = document.getElementById("user-list")
+    data.document_contributors.forEach(contributor => {
+        const holder = `
+                <div class="user-list-item hover:bg-gray-50">
+                    <img src="${contributor.contributor_id.user_avatar}" alt="User 1" class="user-avatar">
+                    <div class="user-info">
+                        <div class="font-semibold">${contributor.contributor_id.email}</div>
+                        <div class="text-gray-500 text-sm">${contributor.role}</div>
+                    </div>
+                </div>
+            </div>`
+
+            user_list.innerHTML += holder
+    });
+}
+
+
+
+quill.on("text-change",(delta, oldDelta, source) => {
+    if(source === "user"){
+        const content = quill.getContents()
+        UpdateDocumentContent(content)
+    }
+})
+
+
+
+
+
+
+CreateDocument()
+FindDocument()
+CanWrite()
+UiElementsAcessControl()
+GetDocumentContributors()
